839ebe85bd7ea9fe175c18452e320fa5094a9748
Jose Alfredo Sitanggang <jose.sitanggang@tiket.com>
2024-12-01 00:48:31 +0700

    interceptors

---
+++ src/core/interceptors/response.interceptor.ts
@@ -0,0 +1,58 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map, catchError, throwError } from 'rxjs';
+
+type Response<T> = {
+  code: number;
+  data: T;
+  text: string;
+};
+
+type ResponseError = {
+  code: number;
+  kind: string;
+  text: string;
+  details?: any;
+};
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<T>,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        const http = context.switchToHttp();
+        const code = http.getResponse().statusCode;
+        return {
+          code: code,
+          data: data,
+          text: code >= 400 ? 'failure' : 'success',
+        };
+      }),
+      catchError((err: Error) =>
+        throwError(() => {
+          const respErr: ResponseError = {
+            code: 500,
+            kind: err.name,
+            text: err.message,
+          };
+          if (err instanceof HttpException) {
+            respErr.code = err.getStatus();
+            const { message } = err.getResponse() as { message: any };
+            if (message) {
+              respErr.details = message;
+            }
+          }
+          throw new HttpException(respErr, respErr.code);
+        }),
+      ),
+    );
+  }
+}
