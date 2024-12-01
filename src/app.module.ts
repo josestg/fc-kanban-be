e1882f8f01f766cfa54df9910994a5947fb27c65
Jose Alfredo Sitanggang <jose.sitanggang@tiket.com>
2024-12-01 14:38:55 +0700

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { CoreModule } from './core/core.module';
+import {  TypeOrmModule } from  '@nestjs/typeorm'
+import { Task } from "./tasks/entities/task.entity";
 
 @Module({
-  imports: [TasksModule, CoreModule],
+  imports: [
+    TasksModule,
+    CoreModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'kanban-be',
+      password: 'kanban-be',
+      database: 'kanban-be',
+      entities: [
+        Task
+      ],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,3 +1,5 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
 export const TASK_STATUSES = [
   'TODO',
   'ON_PROGRESS',
@@ -8,14 +10,30 @@ export const TASK_STATUSES = [
 
 export type TaskStatus = (typeof TASK_STATUSES)[number];
 
+@Entity()
 export class Task {
+  @PrimaryGeneratedColumn({name: 'id'})
   Id: number;
+
+  @Column({type: 'varchar', length: 20, nullable: false})
   title: string;
+
+  @Column({type: 'varchar', length: 255, nullable: false})
   description: string;
+
+  @Column({type: 'enum', enum: TASK_STATUSES, default: 'TODO'})
   status: TaskStatus;
+
+  @Column({type: 'bigint', nullable: false})
   ownerId: number;
+
+  @Column({type: 'bigint', nullable: false})
   createdAt: number; // unix timestamp in millisecond.
+
+  @Column({type: 'bigint', nullable: false})
   updatedAt: number; // unix timestamp in millisecond.
+
+  @Column({type: 'boolean', default: false})
   isDeleted: boolean;
 
   constructor(id: number, title: string, description: string) {
